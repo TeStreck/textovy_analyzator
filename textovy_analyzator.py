d697291661b3a6914e1d5413bd9e1b64cf38000b
TeStreck <162149161+TeStreck@users.noreply.github.com>
2024-03-05 21:06:07 +0100

    Add files via upload

---
+++ textovy_analyzator.py
@@ -0,0 +1,82 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Tereza Streckerová
+email: tereza.vack@gmail.com
+discord: te.str
+"""
+import task_template
+
+
+users = {"bob": "123", "ann": "pas123", "mike": "password123", "liz": "pas123"}
+
+user = input("Vlož přihlašovací jméno: ")
+password = input("Vlož heslo: ")
+
+
+if users.get(user) != password:
+    print("Unregistered user, terminating the program...")
+else:
+    print(f"Welcome to the app, {user}")
+    print("We have 3 texts to be analyzed")
+
+    print("-" * 40)
+
+    number = input("Enter a number btw. 1 and 3 to select: ")
+
+    if number.isalpha():
+        print("This is not a number, terminating program... ")
+
+    elif int(number) not in range(4):
+        print("This number is not in range, terminating program...")
+
+    else:
+        print("-" * 40)
+        selected_text = task_template.TEXTS[int(number) - 1]
+
+
+        # statistiky
+        stat_2 = 0
+        stat_3 = 0
+        stat_4 = 0
+        stat_5 = 0
+        stat_6 = 0
+
+        for word in selected_text.split():
+            if word[0].isupper():
+                stat_2 += 1
+            if word.isupper() is True and word.isalpha() is not True and word.isdigit() is not True:
+                stat_3 += 1
+            if word.islower():
+                stat_4 += 1
+            if word.isdigit():
+                stat_5 += 1
+                stat_6 += int(word) + stat_6
+
+
+        print(f"There are {len(selected_text.split())} words in the selected text.")
+        print(f"There are {stat_2} titlecase words.")
+        print(f"There are {stat_3} uppercase words.")
+        print(f"There are {stat_4} lowercase words.")
+        print(f"There are {stat_5} numeric string.")
+        print(f"The sum of all the numbers {stat_6}.")
+
+        # sloupcovy_graf
+        print("-" * 40)
+        print(f"{"LEN":^3} | {"OCCURENCES":^20}| {"NR."}")
+        print("-" * 40)
+
+        repl_text = selected_text.replace(",", "").replace(".", "")
+
+        freq = [len(word) for word in list(repl_text.split())]
+
+        counts = {}
+        for f_num in freq:
+            if f_num not in counts:
+                counts[f_num] = 1
+            else:
+                counts[f_num] = counts[f_num] + 1
+        for key, value in sorted(counts.items()):
+            occur = "*" * value
+            print(f" {key:^3}| {occur:<20}| {value}")
+
